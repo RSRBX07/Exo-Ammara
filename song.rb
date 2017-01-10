543ae8a250a011282385f9917307ede336301988
Your Nameammara <belamm@hotmail.fr>
2017-01-10 16:30:53 +0100

    chanson

---
+++ song.rb
@@ -0,0 +1,7 @@
+# Ecrire le texte de la chanson
+puts "allongeons la jambe, la jambe"
+puts "car la route est longue"
+ref1 = "Ma poule n'a plus qu'vingt-neuf poussins"
+puts ref1
+ref2 = ref1-1
+puts = "ma poule n'a plus qu'vingt-huit poussins" 
