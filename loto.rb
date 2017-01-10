3baa620c23880f5912c23ff2657e1d15ea651109
Your Nameammara <belamm@hotmail.fr>
2017-01-10 15:06:50 +0100

    cagnote

--- loto.rb
+++ loto.rb
@@ -5,3 +5,14 @@ available_balls= (1..45).to_a
 picked_balls = available_balls.shuffle.take 5
 
 puts" le tirage du jour est : #{picked_balls.sort}"
+
+#Affichage de la cagnote
+#Entre 100 et 500 euros
+# le vendredi 13, la cagnote est de 2millions
+is_vendredi_13 = true
+if is_vendredi_13
+   cagnote = 2.000.000  
+else
+   cagnote = 100.000
+end    
+puts "la cagnote du jour est de #{cagnote}"
