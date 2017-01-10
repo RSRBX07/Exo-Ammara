c88ff9e20e56b025aaf76e866b4f5161f87a83c9
Your Nameammara <belamm@hotmail.fr>
2017-01-10 10:50:42 +0100

    affichage

--- loto.rb
+++ loto.rb
@@ -1,1 +1,7 @@
-# Afficher le tirage du nombre entre 1 et 45
+# Afficher le tirage de 5 nombres entre 1 et 45
+
+available_balls= (1..45).to_a
+#shuffle balls and take 5
+picked_balls = available_balls.shuffle.take 5
+
+puts" le tirage du jour est : #{picked_balls.sort}"
