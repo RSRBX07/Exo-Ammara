5c0460f93faab498e655e334f1250d96b19e14f7
Your Nameammara <belamm@hotmail.fr>
2017-01-11 17:00:43 +0100

    operationnel

--- dice.rb
+++ dice.rb
@@ -7,3 +7,34 @@ return roll
 end
 
 puts roll
+
+
+# resultat d'un d cheated
+def roll_cheated cheated_value
+   return cheated_value
+end
+
+puts roll_cheated 2
+
+
+
+#2eme methode
+def roll
+    1+ rand(6)
+end
+    puts roll
+
+# 3eme methode
+ def roll (cheated_value = nil)
+    if cheated_value==nil
+        return 1+ rand(6)
+    else
+        return cheated_value
+    end    
+end
+
+puts "lancer de de normal"
+puts roll
+
+puts "lancer de de pipe"
+puts roll 4
