f89809f37cdf524496097819bac82f481a51cd8b
Your Nameammara <belamm@hotmail.fr>
2017-01-11 16:06:01 +0100

    loto

--- loto.rb
+++ loto.rb
@@ -20,7 +20,7 @@ end
 puts "la cagnote du jour est de #{cagnote}"
 
 # Afficher si c'est gagner ou perdu
-def demande_de_grille                            #definir une methode demande de grille
+def demande_de_grille                               #definir une methode demande de grille
     puts choisir 5 nombres de 1 à 45
     grid = gets
     return grid
@@ -42,14 +42,20 @@ def input_refinement raw_grid                       # retraitement de la saisie
  grille = demande_de_grille
 
  refined =input_refinement grille
- # Afficher le tirage de 5 nombres entre 1 et 45
- tirage = (1..45).to_a.shuffle.take 5             # tirage est une instance d un tableau 
- tirage = [1, 2, 3, 4, 5]
 
+ draw = make_draw 
 
+ def make_draw                                      #methode de tirage appele make_draw   # Afficher le tirage de 5 nombres entre 1 et 45
+    tirage = (1..45).to_a.shuffle.take 5            # tirage est une instance d un tableau 
+    #cheat
+    tirage = [1, 2, 3, 4, 5]
+    retrun tirage
+ end
+
+.
 #sorting grid and draw
-sorted_grid =refined_grid.sort
-sorted_draw =tirage.sort 
+sorted_grid =refined.sort
+sorted_draw =tirage.sort                           
 
 
  # comaparaison de la grille et le  tirage
