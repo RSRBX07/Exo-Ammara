32fc863dfdb487849160100c3a2fdba5f5397d71
Your Nameammara <belamm@hotmail.fr>
2017-01-12 11:31:20 +0100

    2 examples differents

--- hash.rb
+++ hash.rb
@@ -9,12 +9,22 @@ puts "made with love @ #{place} by #{author}"
 puts  "i love my @ #{children}"
 
 
+# Exercice la ville de naissance
 
 code_postale = {59000=>"Lille" ,75000=>"Paris", 69000=>"Pas de calais"}
-
-
 
 puts "Quel est le CP de ta ville de naissance?"
 cp = gets.to_i
 ville = code_postale[cp]
 puts "ma ville de naissance est #{ville}" 
+
+
+
+# Exercice le code postale
+
+code_postale = {"lille"=>59000, "paris"=> 75000, "pas de calais" => 69000}
+
+puts "quel est ta ville?"
+ville = gets.chomp
+mon_code = code_postale[ville]
+puts "mon cp est #{mon_code}"
