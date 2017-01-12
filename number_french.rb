5d52547eee061b687fa4f11641d0cd96f36275a9
Your Nameammara <belamm@hotmail.fr>
2017-01-12 15:26:58 +0100

    jusqu'a dix-neuf

--- number_french.rb
+++ number_french.rb
@@ -3,33 +3,59 @@
 #qui retour la chine de caractere du chiffre ecrit en toute lettres
 
 def french_number number
-    case number
-    when 0
-         "zero"
-    when 1
-        "un"
-    when 2
-        "deux"
-     when 3
-         "trois"
-    when 6             
-          "six"
-    when 7
-        "sept"
-     when 8
-         "huit"
-     when 9
-         "neuf"
-      else
-          nil
-      end                      
+case number
+when 0
+      "zero"
+when 1
+    "un"
+when 2
+    "deux"
+when 3
+     "trois"
+when 4
+     "quatre"
+when 5
+     "cinq"           
+when 6             
+     "six"
+when 7
+    "sept"
+when 8
+    "huit"
+when 9
+    "neuf"
+else
+      nil
+end                      
 end
-  
-  puts french_number 8
+
+puts french_number 8
 
   
-  def french_number number
-      number = ["un","deux","trois","quatre","cinq","six","cept","huit","neuf"][index] 
-  
+
+
+# retour de chaine de caractere du chiffre
+
+def french_number number
+     number = ["zero","un","deux","trois","quatre","cinq","six","cept","huit","neuf"][number]
 end
 puts french_number 6
+
+
+
+
+# retour de chaine de caractere de 9 à 19
+
+def french_number number
+     number = ["zero","un","deux","trois","quatre","cinq","six","cept","huit","neuf","dix","onze",
+     "douze","treize","quatorze","quinze","seize"],[number]  
+      when 17..19
+      "dix-"+ french_number (number-10)  
+end
+
+puts french_number 15
+
+
+
+
+
