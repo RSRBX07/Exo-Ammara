e4ae44d071650e7c1ee77c662810550488f4fb61
Your Nameammara <belamm@hotmail.fr>
2017-01-12 14:46:55 +0100

    exercice du nombre en tableau

---
+++ number_french.rb
@@ -0,0 +1,35 @@
+# rcrire une methode 
+#qui prend une instance de fixnum en argumenrt (accepte de 0 a 9)
+#qui retour la chine de caractere du chiffre ecrit en toute lettres
+
+def french_number number
+    case number
+    when 0
+         "zero"
+    when 1
+        "un"
+    when 2
+        "deux"
+     when 3
+         "trois"
+    when 6             
+          "six"
+    when 7
+        "sept"
+     when 8
+         "huit"
+     when 9
+         "neuf"
+      else
+          nil
+      end                      
+end
+  
+  puts french_number 8
+
+  
+  def french_number number
+      number = ["un","deux","trois","quatre","cinq","six","cept","huit","neuf"][index] 
+  
+end
+puts french_number 6
