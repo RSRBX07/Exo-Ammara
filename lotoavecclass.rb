441a43ded5e0e44b9737662bd4b929264474e7a7
Your Nameammara <belamm@hotmail.fr>
2017-01-16 15:03:29 +0100

    code de pierre

--- lotoavecclass.rb
+++ lotoavecclass.rb
@@ -1,26 +1,26 @@
-require 'date'
-class Loto
+require 'date'        # pour importer le fichier
+class Loto            # 
 
   def self.get_grid # on définit une méthode self.get_grid de classe : Loto, pour créer la grille = grid
     grid = []
-    5.times do
-      input = gets.to_i
-      grid << input # << = push : pour ajouter dans la grille ce qui a été tapé clavier
+    5.times do  # objet c'est 5 la methode .times pour refaire la methode 5fois'
+      input = gets.to_i 
+      grid << input # << = push : pour ajouter dans la grille ce qui a été tapé clavier, grid c'est la variable local que la methode que comporte le tableau'
     end
-    grid
-   puts "votre grille est : #{grid}" 
+    grid #pour retourner grid pour revoyer la methode puts qui est nil 
+    puts "votre grille est : #{grid}" 
   end
 
   def self.get_flash # flash : methode qui nous donne un tableau de 5 premiers chiffres allant de 1 à 45
-    (1..45).to_a.shuffle.take 5
+    (1..45).to_a.shuffle.take 5 #un range notre methode to_a convertie par array ya lexecution de shuffle de melange q
   end
 
-  def has_winner?
+  def has_winner? #methode de class 
     #comprer tous les bulletins valides avec la grille gagnante
-    sorted_draw = draw.sort
-    @saved_grids.each do |grid|
-      sorted_grid = grid.sort
-      sorted_grid == sorted_draw
+    sorted_draw = draw.sort # on instancier la variable local draw.sort dans sorted_draw une methode de rangement par l mot sort
+    @saved_grids.each do |grid|# variable dinstance applique une methode each avec le paramettre grid cree pour la boucle each
+      sorted_grid = grid.sort # rangement du tableau
+      sorted_grid == sorted_draw #comparaison sorted_grid et sorted_draw sa veut dire true ou false == 
     end
   end
 
