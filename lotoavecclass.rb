d48b8a35edb4b1a85c00522e0703cc12e5188ad9
Your Nameammara <belamm@hotmail.fr>
2017-01-16 15:06:30 +0100

    avec mes commentaire

--- lotoavecclass.rb
+++ lotoavecclass.rb
@@ -26,23 +26,23 @@ class Loto            #
 
   # enregistre une grille
   # pour le loto courant
-  def validate_grid grid
-    @saved_grids ||= []
-    @saved_grids.push grid
+  def validate_grid grid # methode d'instance validate_grid qui prend grid et qui renvoie 
+    @saved_grids ||= []  # on instancier une valeur d'instance pck ya @ et qui est un tableau ou un tableau  vide
+    @saved_grids.push grid # met les information en tableau on utilise une methode push dinstance qui renvoie grid qui ds le tb qui sont saved_grid
   end 
   # demander une grille de jeu
 
   # affichage du montant de la cagnote
   # entre 100 et 500.000 Euros
   # le vendredi 13, la cagnote est de 2 millions
-  def vendredi_13?
-    Date.today.day == 13 and Date.today.friday?
+  def vendredi_13? #methode dinstance
+    Date.today.day == 13 and Date.today.friday? # on appelle methode today qui renvoie lobjet  date et on compare test le jour par and=&&  
   end
 
   
 
   def prize
-    cagnote = if vendredi_13?
+    cagnote = if vendredi_13? tester la methode si true cagnote=200 000 si non100 000
       2_000_000
     else
       100_000
@@ -52,19 +52,18 @@ class Loto            #
   end
 
   def draw
-    available_balls = (1..45).to_a
+    available_balls = (1..45).to_a #methode dinstance draw on utiliseun range pour definir un tableau
     # shuffle balls and take 5
-    @picked_balls ||= available_balls.shuffle.take(5)
+    @picked_balls ||= available_balls.shuffle.take(5)  
 
-    puts "Le tirage du jour est : #{@picked_balls.sort}" 
+    puts "Le tirage du jour est : #{@picked_balls.sort}" #l'affichage du tirage renvoie nil et affiche l'affichage @ picked_balls '
     @picked_balls
   end
 
-  def check_grid grid
+  def check_grid grid #methode qui prend  argument grid
     # afficher si gagne ou perdu
-    if grid.sort ==
-       draw.sort
-      puts "You win !"
+    if grid.sort == draw.sort #on a des instances de tableau qu on va comparer  
+       puts "You win !" # puts renvois derniere expresion évaluer cest le if ou le you win soit you loose mais renvois le nil 
     else
       puts "You loose !"
     end
