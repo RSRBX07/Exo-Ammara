9298b4824c2a5007e11dda4cc77f4af679aafbf1
Your Nameammara <belamm@hotmail.fr>
2017-01-17 14:16:00 +0100

    ma belle song

--- song.rb
+++ song.rb
@@ -1,7 +1,28 @@
 # Ecrire le texte de la chanson
-puts "allongeons la jambe, la jambe"
-puts "car la route est longue"
-ref1 = "Ma poule n'a plus qu'vingt-neuf poussins"
-puts ref1
-ref2 = ref1-1
-puts = "ma poule n'a plus qu'vingt-huit poussins" 
+=begin
+Ma poul' n'a plus qu' vingt-neuf poussins, 
+Ma poul' n'a plus qu' vingt-neuf poussins,
+Elle en avait trente.
+Allongeons la jambe,
+Allongeons la jambe, la jambe
+Car la route est longue.
+Allongeons la jambe, la jambe
+Car la route est longue.
+=end
+iterations=(0..29).to_a.reverse
+iterations.each do |i|
+    if i > 1
+        (1..2).each { puts "Ma poul' n'a plus qu' #{i} poussins" }
+    elsif i == 1
+        (1..2).each { puts "Ma poul' n'a plus qu'un seul poussin" }
+    else
+        (1..2).each { puts "Ma poul' n'a plus aucun poussin" }
+    end
+    puts "Elle en avait trente"
+    puts "Allongeons la jambe,"
+    (1..2).each do
+        puts "Allongeons la jambe, la jambe"
+        puts "Car la route est longue."    
+    end
+    puts
+end
