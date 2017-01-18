1e8f64e2055f5abbff9c6bbbc6a17a92dba94382
Your Nameammara <belamm@hotmail.fr>
2017-01-18 10:54:30 +0100

    .

--- spec/loto_spec.rb
+++ spec/loto_spec.rb
@@ -17,35 +17,39 @@
 
 
 
-require './loto/lotoavecclass.rb'
-RSpec.describe Loto do
-  it 'gets grids' do
+require relative  '../loto/lotoavecclass.rb'      #require relative cest la methode avec largument nom du fichier ./loto/lotoavecclass.rb'
+                                                  #le requirepour atteindre notre fichier nimporte quel endroit dans ruby
+
+RSpec.describe Loto do    # la class RSpec avec la methode describe pour appele la classe de lobjet Loto
+  xit 'gets grids' do     # on a un bloc avec une methode xit pour ignorer le test il reste en attente
     Loto.get_grid
   end
-  it'give a radom flash grid'do
-    expect(Loto.get_flash).not_to be_nil
-    expect(Loto.get_flash).to be_a Array
-    expect(Loto.get_flash).to equal 5
-    expect(Loto.get_flash).to = eql Loto.get_flash 
-end  
-  it'make a draw'do
-    loto = Loto.new
-    loto_draw = loto.draw
-    expect(loto_draw).respond_to:draw
-    expect(loto_draw).not_to be_nil
-    expect(loto_draw).to be_a Array
-    expect(loto_draw).to equal 5
-    expect(loto_draw).to = eql Loto.draw
 
- end
-  it 'validate a grid registre this grid' do
-   loto = Loto.new
-   grid = [1,2,3,4,5]
-   validate_grid = loto.validate_grid grid
-   expect (validate_grid).to include grid
+  it 'give a radom flash grid' do #on appelle une methode it dans un bloc 4fois de la classe LOto
+    expect(Loto.get_flash).not_to be_nil   #renvoi un nil
+    expect(Loto.get_flash).to be_a #Array renvoi un tableau
+    expect(Loto.get_flash.size).to              #equal 5 renvoi si le equal lidentification et egal a 5 nombres
+    expect(Loto.get_flash).not_to eql Loto.get_flash  #renvoi il faut pas que get flash renvoi le meme resultat
+  end
+
+  it 'make a draw' do #on appele une methode it dans un bloc describe do avec une chaine de caratere 'make a draw' 
+    loto = Loto.new #instancier la classe Loto.new dans loto
+    loto_draw = loto.draw # on appelle la methode loto.draw
+    expect(loto).to respond_to :draw 
+    expect(loto_draw).not_to be_nil # on test sil renvoi un nil
+    expect(loto_draw).to be_a Array #Array renvoi un tableau
+    expect(loto_draw.size).to equal 5  #equal 5 renvoi si le equal lidentification et egal a 5 nombres
+    expect(loto_draw).to eql loto.draw # on testcest le resultat est egal a tableau  
+  end
+  it 'validate a grid register this grid' do
+     loto = Loto.new instancier une class Loto.new dans loto
+     grid = [1,2,3,4,5] # grid variable contien un objet qui un tableau
+     validated_grids = loto.validate_grid grid  # validate_grid on lui assigne une methode dinstance 
+     expect (validated_grids).to inclued grid   #qui test grid sil est inclue dans validate_grids ou plutot validated grids contien grid
+end
+  it 'reject grid validation after draw' do           #on test si la grille est rejetée  apres  le tirage 
   
-  end  
- end
+end
 
 
 #loto_du_samedi = Loto.new
