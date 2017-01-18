f9fdc3994b4190cd6f8905a6cc8f5389ab04a0ab
Your Nameammara <belamm@hotmail.fr>
2017-01-18 16:37:53 +0100

    new vehicle

--- vehicle.rb
+++ vehicle.rb
@@ -1,8 +1,29 @@
 class Vehicle
   attr_reader :position
-  def move
-    puts"i am going
+  attr_reader :out_of_order
+
+  def self.mon_new
+    puts 'im creating a new vehicle'
+    Object.new
+  end  
+
+
+  def initialize
+    @position = 'roubaix'
+    @out_of_order = false
+  end   
+
+  def move (destination)
+    puts 'changement de position'
+    @position = destination
+   
   end
-    def initialze
-      @position = "roubaix"
-    end
+  peugeot = Vehicle.mon_new  
+end
+
+
+#tahiti='tahiti'
+# puts (citroen = Vehicle.new).inspect
+# citroen.position
+# puts citroen.inspect
+# citroen.move(tahiti)
