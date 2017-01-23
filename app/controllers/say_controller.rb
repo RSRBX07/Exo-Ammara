9bf70d8bb575f4f5ae9ce5ccd784f2f13290cbac
Your Nameammara <belamm@hotmail.fr>
2017-01-23 11:54:09 +0100

    rails g controller Say hello good bay

---
+++ app/controllers/say_controller.rb
@@ -0,0 +1,10 @@
+class SayController < ApplicationController
+  def hello
+  end
+
+  def good
+  end
+
+  def bay
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'say/hello'
+
+  get 'say/good'
+
+  get 'say/bay'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
